c8198b7b022ca9eaa52fa5ec05b62755bb88776f
Ravi Ranjan <raviranjan.sjbit@gmail.com>
2018-06-01 11:05:07 +0530

    added installation file for dlib

--- recognize.py
+++ recognize.py
@@ -2,18 +2,12 @@ import dlib
 import scipy.misc
 import numpy as np
 import os
-
 face_detector = dlib.get_frontal_face_detector()
 
 shape_predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 face_recognition_model = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
 
-# This is the tolerance for face comparisons
-# The lower the number - the stricter the comparison
-# To avoid false matches, use lower value
-# To avoid false negatives (i.e. faces of the same person doesn't match), use higher value
-# 0.5-0.6 works well
 TOLERANCE = 0.5
 
 def get_face_encodings(path_to_image):
@@ -26,6 +20,7 @@ def get_face_encodings(path_to_image):
     return [np.array(face_recognition_model.compute_face_descriptor(image, face_pose, 1)) for face_pose in shapes_faces]
 
 def compare_face_encodings(known_faces, face):
+    #print( face)
     return (np.linalg.norm(known_faces - face, axis=1) <= TOLERANCE)
 
 def find_match(known_faces, names, face):
